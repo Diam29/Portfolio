be7f539d681741e399f5f12bc17b164e8c7da20e
Diamela <97529607+Diam29@users.noreply.github.com>
2023-09-14 18:16:53 -0300

    Actualizar Resume.jsx

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -54,7 +54,7 @@ const Resume = () => {
             />
             <div className='timeline'>
                 <div className='timeline__experience'>
-                    <h3 className='timeline__experience__header-text'>Experience</h3>
+                    <h3 className='timeline__experience__header-text'>Education</h3>
                     <VerticalTimeline
                         layout={'1-column'}
                         lineColor='var(--yellow-theme-main-color)'
@@ -89,7 +89,7 @@ const Resume = () => {
                     </VerticalTimeline>
                 </div>
                 <div className='timeline__education'>
-                    <h3 className='timeline__education__header-text'>Education</h3>
+                    <h3 className='timeline__education__header-text'>Experience</h3>
                     <VerticalTimeline
                         layout={'1-column'}
                         lineColor='var(--yellow-theme-main-color)'
